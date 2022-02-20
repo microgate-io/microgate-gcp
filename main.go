67f49b756876c189697a35470f7f53791dd3a3b6
Ernest Micklei <ernest.micklei@gmail.com>
2022-02-20 15:43:28 +0100

    work on pubsub

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"context"
+
+	"github.com/emicklei/microgate-io/microgate-gcp/v1/queue"
 	"github.com/microgate-io/microgate"
 	apilog "github.com/microgate-io/microgate-lib-go/v1/log"
 	"github.com/microgate-io/microgate/v1/config"
 	mconfig "github.com/microgate-io/microgate/v1/config"
 	mlog "github.com/microgate-io/microgate/v1/log"
-	mqueue "github.com/microgate-io/microgate/v1/queue"
 )
 
 func main() {
 	mlog.Init()
 
 	gateConfig := config.Load("config.yaml")
-	apilog.GlobalDebug = gateConfig.FindBool("global_debug")
+	apilog.GlobalDebug, _ = gateConfig.FindBool("global_debug")
+
+	qService, err := queue.NewQueueingServiceImpl(gateConfig)
+	if err != nil {
+		mlog.Fatalw(context.Background(), "cannot create queueing service", "err", err)
+	}
 
 	// these are the gRPC services provided to the backend
 	provider := microgate.ServiceProvider{
 		Log:      mlog.NewLogService(),
 		Config:   mconfig.NewConfigServiceImpl(),
-		Queueing: mqueue.NewQueueingServiceImpl(gateConfig),
+		Queueing: qService,
 	}
 
 	microgate.Start(gateConfig, provider)
--- v1/queue/service.go
+++ v1/queue/service.go
@@ -2,20 +2,78 @@ package queue
 
 import (
 	"context"
+	"sync"
 
+	"cloud.google.com/go/pubsub"
+	"github.com/emicklei/tre"
+	"github.com/emicklei/xconnect"
 	apiqueue "github.com/microgate-io/microgate-lib-go/v1/queue"
 	mlog "github.com/microgate-io/microgate/v1/log"
 )
 
+// QueueingServiceImpl is used to publish or receive messages via GCP Pub/Sub
+// https://pkg.go.dev/cloud.google.com/go/pubsub#Topic
 type QueueingServiceImpl struct {
 	apiqueue.UnimplementedQueueingServiceServer
+	topics        map[string]*pubsub.Topic        // one service can publish to multiple topics
+	subscriptions map[string]*pubsub.Subscription // one service can receive from multiple subscriptions
+	pubsubClient  *pubsub.Client
+	mutex         *sync.RWMutex
+}
+
+func NewQueueingServiceImpl(config xconnect.Document) (*QueueingServiceImpl, error) {
+	p, err := config.FindString("project_id")
+	if err != nil {
+		return nil, tre.New(err, "missing config paramater [project_id]")
+	}
+	ctx := context.Background()
+	pc, err := pubsub.NewClient(ctx, p)
+	if err != nil {
+		return nil, tre.New(err, "cannot create pubsub client", "project_id", p)
+	}
+	return &QueueingServiceImpl{
+		topics:        map[string]*pubsub.Topic{},
+		subscriptions: map[string]*pubsub.Subscription{},
+		pubsubClient:  pc,
+		mutex:         new(sync.RWMutex)}, nil
 }
 
 func (s *QueueingServiceImpl) Publish(ctx context.Context, req *apiqueue.PublishRequest) (*apiqueue.PublishResponse, error) {
 	mlog.Debugw(ctx, "Publish", "req", req)
-	return nil, nil
+	s.mutex.RLock()
+	top, ok := s.topics[req.TopicId]
+	s.mutex.RUnlock()
+	if !ok {
+		// first time, create the topic (reference to the GCP resource)
+		s.mutex.Lock()
+		top = s.pubsubClient.Topic(req.TopicId)
+		s.topics[req.TopicId] = top
+		s.mutex.Unlock()
+	}
+	_ = top.Publish(ctx, &pubsub.Message{Data: req.Message})
+	return new(apiqueue.PublishResponse), nil
 }
 func (s *QueueingServiceImpl) Subscribe(ctx context.Context, req *apiqueue.SubscribeRequest) (*apiqueue.SubscribeResponse, error) {
 	mlog.Debugw(ctx, "Subscribe", "req", req)
-	return nil, nil
+	s.mutex.RLock()
+	_, ok := s.subscriptions[req.SubscriptionId]
+	s.mutex.RUnlock()
+	if !ok {
+		// first time, create the subscription (reference to the GCP resource)
+		s.mutex.Lock()
+		sub := s.pubsubClient.Subscription(req.SubscriptionId)
+		s.subscriptions[req.SubscriptionId] = sub
+		s.mutex.Unlock()
+		// start receiving
+		err := sub.Receive(context.Background(), func(ctx context.Context, m *pubsub.Message) {
+			mlog.Debugw(ctx, "received message", "subscription", req.SubscriptionId, "data", m.Data)
+			m.Ack()
+		})
+		if err != nil {
+			return nil, tre.New(err, "cannot create receive from subscription", "subscription", req.SubscriptionId)
+		}
+	}
+	return new(apiqueue.SubscribeResponse), nil
 }
+
+func (s *QueueingServiceImpl) Shutdown() error { return nil }

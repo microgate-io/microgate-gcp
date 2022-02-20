a4afd03671c6d1d3a2e1412f912a7aec2c3a1ef2
Ernest Micklei <ernest.micklei@gmail.com>
2022-02-20 15:56:54 +0100

    add psm

--- v1/queue/service.go
+++ v1/queue/service.go
@@ -77,3 +77,14 @@ func (s *QueueingServiceImpl) Subscribe(ctx context.Context, req *apiqueue.Subsc
 }
 
 func (s *QueueingServiceImpl) Shutdown() error { return nil }
+
+// PubSubMessage is the payload of a Pub/Sub event.
+// See the documentation for more details:
+// https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
+type PubSubMessage struct {
+	Message struct {
+		Data []byte `json:"data,omitempty"`
+		ID   string `json:"id"`
+	} `json:"message"`
+	Subscription string `json:"subscription"`
+}
